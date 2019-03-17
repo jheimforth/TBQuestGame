904fe7ae27f5992bc1f70b34383b8debb0b0f1c5
jared heimforth <jheimforth78@live.com>
2019-03-17 18:29:37 -0400

    S2 Upload

---
+++ TBQuestGame.S2/PresentationLayer/GameSessionViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TBQuestGame.Models;
+using TBQuestGame.S2.Models;
+using System.Collections.ObjectModel;
+
+namespace TBQuestGame.PresentationLayer
+{
+    public class GameSessionViewModel
+    {
+        #region Fields
+        private Player _player;
+        private List<string> _messages;
+        private Map _gameMap;
+        private Location _currentLocation;
+        private string _currentLocationName;
+        private ObservableCollection<Location> _accessibleLocations;
+
+       
+
+
+        #endregion
+
+        #region Properties
+        public Player Player
+        {
+            get { return _player; }
+            set { _player = value; }
+        }
+
+        public string MessageDisplay
+        {
+            get { return string.Join("\n\n", _messages); }
+            
+        }
+
+
+        public Map GameMap
+        {
+            get { return _gameMap; }
+            set { _gameMap = value; }
+        }
+
+        public Location CurrentLocation
+        {
+            get { return _currentLocation; }
+            set { _currentLocation = value; }
+        }
+
+        public string CurrentLocationName
+        {
+            get { return _currentLocationName; }
+            set { _currentLocationName = value; }
+        }
+
+        public ObservableCollection<Location> AccessibleLocations
+        {
+            get { return _accessibleLocations; }
+            set { _accessibleLocations = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+        public GameSessionViewModel()
+        {
+
+        }
+
+        public GameSessionViewModel(Player player, Map gameMap, Location currentLocation)
+        {
+            _player = player;
+            _gameMap = gameMap;
+            _currentLocation = currentLocation;
+            InitializeView();
+        }
+
+        
+        private void InitializeView()
+        {
+            _accessibleLocations = _gameMap.AccessibleLocations;
+        }
+
+
+
+        #endregion
+
+
+    }
+}
